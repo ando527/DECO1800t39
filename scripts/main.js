67b7f01a056b0cfc7a57700dd228b37f5baef712
ricardothesillyllama <164969626+ricardothesillyllama@users.noreply.github.com>
2024-09-10 19:58:17 +1000

    Implemented some of Dataset 1 into the map

--- scripts/main.js
+++ scripts/main.js
@@ -7,4 +7,34 @@ $( document ).ready(function() {
 
     var marker = L.marker([-27.4959193, 153.0117005]).addTo(map);
 
+    var data = {
+        resource_id: "brisbane-parking-meters",
+    };
+
+    $.ajax({
+        url: "https://data.brisbane.qld.gov.au/api/explore/v2.1/catalog/datasets/brisbane-parking-meters/records?limit=100",
+        data: data,
+        dataType: "jsonp", 
+        cache: true,
+        success: function(data) {
+            iterateRecords(data, map); 
+        }
+    });
 });
+
+function iterateRecords(data, map) { 
+    console.log(data); 
+
+    $.each(data.results, function(recordID, recordValue) {
+        console.log(recordValue);
+
+        var recordLatitude = recordValue["latitude"];
+        var recordLongitude = recordValue["longitude"];
+
+        if (recordLatitude && recordLongitude) {
+            var marker = L.marker([recordLatitude, recordLongitude]).addTo(map); 
+            var popupText = "";
+            marker.bindPopup(popupText).openPopup();
+        }
+    });
+}
