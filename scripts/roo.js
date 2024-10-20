6a5329516318383b0ecaa3b83713d09b3cab9878
ando527 <43904.ando527@gmail.com>
2024-10-20 15:00:52 +1000

    finalised first draft of currency system.

--- scripts/roo.js
+++ scripts/roo.js
@@ -2,20 +2,128 @@ var shoesUrl;
 var topUrl;
 var hatUrl;
 var balance = 0;
+var owned = [];
+var lookingAt = "";
+
+const productMapping = {
+    "shoes": "Sneakers",
+    "slippers": "Slippers",
+    "jetShoes": "Jet Shoes!",
+    "boots": "Ugg Boots",
+    "jetpack": "Robot Suit/Jet Pack",
+    "jacket": "Dinner Jacket",
+    "formal": "Formal Shirt",
+    "tshirt": "T-Shirt",
+    "singlet": "Singlet",
+    "earHoop": "Hoop Earring",
+    "headBand": "Sweat Band",
+    "diamondEarring": "Diamond Earring",
+    "baseballCap": "Baseball Cap",
+    "bucketHatSide": "Bucket Hat",
+    "headphone": "Headphones"
+}
+
+const productPrices = {
+    "shoes": 30,
+    "slippers": 20,
+    "boots": 40,
+    "jetShoes": 100,
+    "jetpack": 200,
+    "jacket": 40,
+    "formal": 30,
+    "tshirt": 30,
+    "earHoop": 20,
+    "headBand": 25,
+    "diamondEarring": 50,
+    "baseballCap": 20,
+    "bucketHatSide": 20,
+    "headphone": 60
+}
+
+function exitCheckout(){
+    document.querySelector('.checkout').style.display = "none";
+    document.querySelector('.checkout').style.opacity = 0;
+}
+
+function buyItem(){
+    if (lookingAt == ""){
+        return;
+    }
+    
+    if (balance >= productPrices[lookingAt]){
+        balance -= productPrices[lookingAt];
+        owned.push(lookingAt);
+        unlockItem(lookingAt);
+        localStorage.setItem("owned", JSON.stringify(owned));
+        document.querySelector(".balance").innerHTML = balance;
+        localStorage.setItem("balance", balance);
+    }
+
+    lookingAt = "";
+    exitCheckout();
+}
 
 function loadShoe(e){
-    document.querySelector("#shoesImg").src= "images/shoes/" + e.dataset.shoe + ".png";
-    localStorage.setItem("shoe", ("images/shoes/" + e.dataset.shoe + ".png"));
+    if (owned.includes(e.dataset.shoe) || e.dataset.shoe == "roo_bottom"){
+        document.querySelector("#shoesImg").src= "images/shoes/" + e.dataset.shoe + ".png";
+        localStorage.setItem("shoe", ("images/shoes/" + e.dataset.shoe + ".png"));
+    } else {
+        document.querySelector('.productName').textContent = productMapping[e.dataset.shoe];
+        document.querySelector('.productPrice').textContent = "" + productPrices[e.dataset.shoe];
+        document.querySelector('.productImage').src = `images/shoes/${e.dataset.shoe}.png`;
+        lookingAt = e.dataset.shoe;
+        if (productPrices[lookingAt] > balance){
+            document.querySelector('.checkoutButton').classList.add('greyedOut');
+            document.querySelector('.checkoutButton').textContent = "Can't Afford these yet...";
+        } else {
+            document.querySelector('.checkoutButton').classList.remove('greyedOut');
+            document.querySelector('.checkoutButton').textContent = "Buy this item!";
+        }
+        document.querySelector('.checkout').style.display = "flex";
+        document.querySelector('.checkout').style.opacity = 1;
+    }
 }
 
 function loadTop(e){
-    document.querySelector("#topImg").src= "images/tops/" + e.dataset.top + ".png";
-    localStorage.setItem("top", ("images/tops/" + e.dataset.top + ".png"));
+    if (owned.includes(e.dataset.top) || e.dataset.top == "roo_mid"){
+        document.querySelector("#topImg").src= "images/tops/" + e.dataset.top + ".png";
+        localStorage.setItem("top", ("images/tops/" + e.dataset.top + ".png"));
+    } else {
+        document.querySelector('.productName').textContent = productMapping[e.dataset.top];
+        document.querySelector('.productPrice').textContent = "" + productPrices[e.dataset.top];
+        document.querySelector('.productImage').src = `images/tops/${e.dataset.top}.png`;
+        lookingAt = e.dataset.top;
+        if (productPrices[lookingAt] > balance){
+            document.querySelector('.checkoutButton').classList.add('greyedOut');
+            document.querySelector('.checkoutButton').textContent = "Can't Afford these yet...";
+        } else {
+            document.querySelector('.checkoutButton').classList.remove('greyedOut');
+            document.querySelector('.checkoutButton').textContent = "Buy this item!";
+        }
+        document.querySelector('.checkout').style.display = "flex";
+        document.querySelector('.checkout').style.opacity = 1;
+    }
 }
 
 function loadHat(e){
+    if (owned.includes(e.dataset.hat) || e.dataset.top == "roo_top"){
     document.querySelector("#hatImg").src= "images/hats/" + e.dataset.hat + ".png";
     localStorage.setItem("hat", ("images/hats/" + e.dataset.hat + ".png"));
+    } else {
+        document.querySelector('.productName').textContent = productMapping[e.dataset.hat];
+        document.querySelector('.productPrice').textContent = "" + productPrices[e.dataset.hat];
+        document.querySelector('.productImage').src = `images/hats/${e.dataset.hat}.png`;
+        lookingAt = e.dataset.hat;
+        if (productPrices[lookingAt] > balance){
+            document.querySelector('.checkoutButton').classList.add('greyedOut');
+            document.querySelector('.checkoutButton').textContent = "Can't Afford these yet...";
+        } else {
+            document.querySelector('.checkoutButton').classList.remove('greyedOut');
+            document.querySelector('.checkoutButton').textContent = "Buy this item!";
+        }
+        document.querySelector('.checkout').style.display = "flex";
+        document.querySelector('.checkout').style.opacity = 1;
+    }
 }
 
 $(document).ready(function(){
@@ -52,5 +160,25 @@ $(document).ready(function(){
         localStorage.setItem("balance", balance);
     }
 
+    owned = localStorage.getItem("owned");
+    if (owned){
+        owned = JSON.parse(owned);
+        for (item of owned){
+            unlockItem(item);
+            console.log(item);
+        }
+    } else {
+        owned = []
+        localStorage.setItem("owned", JSON.stringify(owned));
+    }
+
   });
 
+  function unlockItem(item) {
+    let thumbs = document.querySelectorAll(`.${item}`);
+    thumbs.forEach(thumb => {
+        thumb.textContent = "Owned";
+        thumb.classList.add('bought');
+    });
+    console.log(thumbs);
+}
